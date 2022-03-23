2a652c20f0037f67f09c49723df694b83c23fb2c
Rafael Tavares <rafael@ibinetwork.com.br>
2022-03-23 18:10:03 -0300

    Update _frontend.sh

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -137,7 +137,7 @@ server {
   server_name $frontend_hostname;
 
   location / {
-    proxy_pass http://127.0.0.1:3000;
+    proxy_pass http://127.0.0.1:3333;
     proxy_http_version 1.1;
     proxy_set_header Upgrade \$http_upgrade;
     proxy_set_header Connection 'upgrade';
